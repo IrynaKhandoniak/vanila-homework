107eb1f6cb01a0207eb0a60d13bab7ecdb7f7299
Iryna Khandoniak <irr.enn.booking@gmail.com>
2024-09-10 12:49:41 +0100

    updated js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function refreshWeather(response) {
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.humidity}%`;  
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;  
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
