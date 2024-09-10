c535f1b009631014963bf2b175e806f966cd260d
Iryna Khandoniak <irr.enn.booking@gmail.com>
2024-09-10 10:21:23 +0100

    updated search engine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+let apiKey = "42b343t801425off902ba1fa3b6ffd4f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metrics`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
